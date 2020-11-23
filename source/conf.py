d7dfae26d34a492c8ce6a4e6a0ba7f77f320da65
John <john@johntelford.com>
2020-11-23 15:10:24 -0800

    failed - local toc

--- source/conf.py
+++ source/conf.py
@@ -196,7 +196,7 @@ author = u'John Telford'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.9'
+version = '0.2.10'
 # The full version, including alpha/beta/rc tags.
 release = ''
 
